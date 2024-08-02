8762467d0b732caf3c9591764c3016a52eedbd72
ritesh <riteshbehera123@gmail.com>
2024-08-02 15:45:49 +0530

    Fix env data

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 8080;
 
+
+app.get('/ping', (req, res) => {
+    res.send('PONG')
+})
 app.listen(PORT, () => {
     console.log(`Server is running at ${PORT}`);
 })
